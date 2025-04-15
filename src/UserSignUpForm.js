26e0f86af950b69eb275999b80d8424a10b2047f
rudreshmishra25 <64805407+rudreshmishra25@users.noreply.github.com>
2025-04-15 21:52:18 +0530

    commit

--- src/UserSignUpForm.js
+++ src/UserSignUpForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function UserSignUpForm() {
-  return <div className='container'>
+  return <div className='d-flex justify-content-center align-items-center vh-100'>
   <h1>Hii This is SignUp form </h1>
   </div>;
 }
