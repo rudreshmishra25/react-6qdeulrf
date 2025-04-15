ef2f6363c20ec64ee4b28383637403f7ea5f196c
rudreshmishra25 <64805407+rudreshmishra25@users.noreply.github.com>
2025-04-15 21:41:00 +0530

    first commit

--- src/UserSignUpForm.js
+++ src/UserSignUpForm.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function UserSignUpForm() {
-  return <h1>Hii This is SignUp form </h1>;
+  return <div className='container'>
+  <h1>Hii This is SignUp form </h1>
+  </div>;
 }
